e2e0c0e72b6df5ab25208e42aceb0a2680e3e145
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-03-19 20:21:05 +0530

    Done

--- Assignments HTML, CSS/Assignment 20 js/3_swap_values.js
+++ Assignments HTML, CSS/Assignment 20 js/3_swap_values.js
@@ -1,11 +1,6 @@
 function swap(...values){
     
     let [y,x] = values;
-
-    // values.reverse()     // this will give the result but!
-    // values = []; values.push(x,y);    // this will also give the result
-
-    // return Array.from([x,y].join(",").split(",").map(Number));
     return [x,y];
 }
 
