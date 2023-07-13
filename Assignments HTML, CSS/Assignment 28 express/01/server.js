278632295092c5b56405722d6d1535621443b32b
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-07-13 17:07:28 +0530

    Assignment

--- Assignments HTML, CSS/Assignment 28 express/01/server.js
+++ Assignments HTML, CSS/Assignment 28 express/01/server.js
@@ -1,15 +1,85 @@
-const mongoosh = require('mongoosh');
+const mongoose = require('mongoose');
 const http = require('http');
 
 const connect = ()=>{
-    return mongoosh.connect('mongodb://127.0.0.1:27017/products');
+    return mongoose.connect('mongodb://127.0.0.1:27017/products');
 }
+
+const productSchema = mongoose.Schema({
+    id: {type:Number, required:true},
+    name: {type:String, required:true},
+    brand: {type:String, required:true},
+    price: {type:Number, required:true},
+    color: {type:String, required:true},
+    size: {type:String, required:true},
+    gender: {type:String, required:true},
+    category:{type:String, required:true}
+}, {
+    versionKey:false,
+});
+
+const Pro_info = mongoose.model('pro_info',productSchema);
 
 const server = http.createServer( (req,res)=>{
     if(req.url == '/'){
         res.statusCode = 200;
-        res.setHeader = 'Content-Type: text/html';
-        res.end('<h1>We:come to Men & Women Dummy Data</h1>')
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>Welcome to Men & Women Dummy Data</h1>')
+    }
+
+    else if(req.url == '/men'){
+
+        try{
+
+            async function getData(){
+                let pro_info = await Pro_info.find({gender:'Male'}, {_id:0, gender:0}).limit(10).lean().exec();
+                
+                console.log(pro_info);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(pro_info));
+            }
+
+            getData();
+
+        }catch(err){
+            res.end('Some Error Occured');
+        }
+        
+    }
+
+    else if(req.url == '/women'){
+
+        try{
+
+            async function getData(){
+                let pro_info = await Pro_info.find({gender:'Female'}, {_id:0, gender:0}).limit(10).lean().exec();
+                
+                console.log(pro_info);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(pro_info));
+            }
+
+            getData();
+
+        }catch(err){
+            res.end('Some Error Occured');
+        }
+        
+    }
+
+    else if(req.url == '/other'){
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1 style="color:red">page not found</h1>')
+
+    } 
+    
+    else {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1 style="color:red">page not found</h1>')
     }
 })
 
