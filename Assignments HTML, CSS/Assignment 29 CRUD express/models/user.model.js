a314e2f759255d79de8d42768177a6ba3fb8402b
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-07-15 20:55:08 +0530

    Assignment

---
+++ Assignments HTML, CSS/Assignment 29 CRUD express/models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true,
+        maxLength: [30, 'Name must be less than 30 chars']
+    },
+    
+    email:{
+        type: String,
+        required : [true, 'Email is required'],
+        unique : true
+    },
+
+    password:{
+        type: String,
+        required : [true, 'Password is Required'],
+    }
+}, {
+    versionKey:false
+})
+
+module.exports = mongoose.model('user', userSchema)
