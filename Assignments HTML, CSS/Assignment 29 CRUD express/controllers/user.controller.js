0ce6422519d6d273621437ce6e55c4b2142ad2b7
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-07-16 18:48:46 +0530

    Assignment

--- Assignments HTML, CSS/Assignment 29 CRUD express/controllers/user.controller.js
+++ Assignments HTML, CSS/Assignment 29 CRUD express/controllers/user.controller.js
@@ -2,12 +2,55 @@ const User = require('../models/user.model');
 
 exports.register = async (req, res) =>{
     try {
+        const {name,email,password} = req.body;
+
+
+        if(!name || !email || !password){
+            return res.send('Name, Email, and Password are required');
+        }
+
+        const userExists = await User.findOne({email}); 
         
-        
+        if(userExists){
+            throw new Error('User already exists')
+        }
+
         const user = await User.create(req.body);
-        res.status(200).json({
+        res.status(201).json({
             msg: 'User Registered Successfully'
         })
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+exports.login = async (req, res) =>{
+    try {
+
+        const {email,pass} = req.body;
+
+        if(!email || !pass){
+            throw new Error('Please enter vaild data');
+        }
+
+        const userExists = await User.findOne({email}); 
+        
+        if(!userExists){
+            return res.status(400).json({
+                msg: 'No User Found'
+            })
+        }
+
+        if(userExists.password == pass){
+            return res.status(200).json({
+                msg: 'User Login Successfully'
+            });
+        }else{
+            return res.status(400).json({
+                msg: 'Check your password and try again'
+            }); 
+        }
 
     } catch (err) {
         console.log(err);
