80d61969aa49992ff6f06185d102aee563e2f4b1
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-08-09 15:57:40 +0530

    Practice

--- Assignments HTML, CSS, JS, BACKEND, REACT/Assignment 32 React/2/src/App.jsx
+++ Assignments HTML, CSS, JS, BACKEND, REACT/Assignment 32 React/2/src/App.jsx
@@ -1,7 +1,30 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [count, setCount] = useState(0);
+
+  function handleAdd(){
+    setCount(count + 1);
+}
+
+function handleSub(){
+    if(count > 0){
+
+        setCount(count - 1);    
+    }
+  }
+
+  return (
+    <div className='container'>
+        <h1>{count}</h1>
+
+        <div className='btn'>
+            <button onClick={handleAdd}>Increment</button>
+            <button onClick={handleSub}>Decrement</button>
+        </div>
+    </div>
+  )
 }
 
 export default App
